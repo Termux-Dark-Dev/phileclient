832d85d260223efafaa79a3dbc703b6d67d28949
Vishal <vishalmspandey@gmail.com>
2023-04-25 00:01:59 +0530

    Added feature to connect to backend for writing the order to spreadsheet

--- lib/apiname.dart
+++ lib/apiname.dart
@@ -1,5 +1,6 @@
 class APIENDPOINTNAME {
-  static String HOME = "http://68.183.93.42:8999/api/";
+  static String HOME = "https://phile.in:8999/api/";
+  // static String HOME = "http://68.183.93.42:8999/api/";
   // static String HOME = "https://philebackendapp.onrender.com/api/";
   // static String HOME = "http://192.168.0.103:8000/api/";
   static String CREATEUSER = HOME + "adduser/";
@@ -16,4 +17,5 @@ class APIENDPOINTNAME {
   static String FORGOTPASSCHECKER = HOME + "forgotpasschecker/";
   static String FORGOTPASSCHANGE = HOME + "forgotpasschange/";
   static String RAISEQUERY = HOME + "raisequery/";
+  static String ADDORDERTOSPREADSHEET = HOME + "storetosheet/";
 }
--- lib/services/bookorder/bookordersrvc.dart
+++ lib/services/bookorder/bookordersrvc.dart
@@ -1,9 +1,33 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:http/http.dart' as http;
 import 'package:phileclientapp/apiname.dart';
 
 class BookOrderService {
+  Future<void> addOrderInSpreadSheet(List listofsrvc, String datetime,
+      String userid, String storeid, String orderid) async {
+    try {
+      var body = {
+        "serviceopted": listofsrvc.toString(),
+        "datetime": datetime,
+        "userid": userid,
+        "storeid": storeid,
+        "orderid": orderid
+      };
+
+      var response = await http.post(
+          Uri.parse(APIENDPOINTNAME.ADDORDERTOSPREADSHEET),
+          body: body,
+          headers: {
+            HttpHeaders.contentTypeHeader: 'application/x-www-form-urlencoded',
+          });
+      return;
+    } catch (e) {
+      return;
+    }
+  }
+
   Future bookService(List listofsrvc, String selectedtime, String selecteddate,
       String userid, String storeid, String orderdate) async {
     try {
@@ -21,6 +45,8 @@ class BookOrderService {
           body: datatobesent);
 
       if (response.statusCode == 201) {
+        await addOrderInSpreadSheet(listofsrvc, selecteddate + selectedtime,
+            userid, storeid, jsonDecode(response.body)["orderid"].toString());
         return true;
       } else {
         return false;
